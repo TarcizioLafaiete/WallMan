8427628986e9d3e07820a31887f63154270a2cf5
TarcizioLafaiete <tarcizio-augusto@hotmail.com>
2024-09-17 19:14:40 -0300

    refactor: Adequando a classe ao novo layout da aplicação  PS: Também avancei no processo de envio dos dados de arquivos e diretorios para o controller

--- manager/view/mainwindow.py
+++ manager/view/mainwindow.py
@@ -1,14 +1,20 @@
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtWidgets import QMainWindow, QFileDialog
 from PySide6.QtGui import QIcon
-from .forms.ui_mainwindow import Ui_MainWindow
 from PySide6.QtCore import Signal,Slot
+
+from .forms.ui_mainwindow import Ui_MainWindow
+from business.utils import pathOperationType
+
 import json
+
 
 class mainWindow(QMainWindow):
 
     startWorker = Signal(dict)
     closeWorker = Signal()
     saveConfig = Signal()
+    pathOperation = Signal(pathOperationType,str)
+    
 
     def __init__(self):
         super().__init__()
@@ -19,19 +25,23 @@ class mainWindow(QMainWindow):
         self.ui.start_button.setStyleSheet("background-color: #00AA00")
         self.ui.close_button.setStyleSheet("background-color: #AA0000")
 
-        self.__setIcons(self.ui.newImages_folder,'view/forms/resources/adicionar-pasta.png')
-        self.__setIcons(self.ui.ignoreImages_folder,'view/forms/resources/adicionar-pasta.png')
-        self.__setIcons(self.ui.nsfw_folder,'view/forms/resources/adicionar-pasta.png')
+        self.__setIcons(self.ui.add_folder,'view/forms/resources/adicionar-pasta.png')
+        self.__setIcons(self.ui.ignore_folder,'view/forms/resources/adicionar-pasta.png')
+        self.__setIcons(self.ui.remove_folder,'view/forms/resources/remover-pasta.png')
 
-        self.__setIcons(self.ui.newImages_files,'view/forms/resources/adicionar-imagem.png')
-        self.__setIcons(self.ui.ignoreImages_files,'view/forms/resources/adicionar-imagem.png')
-        self.__setIcons(self.ui.nsfw_files,'view/forms/resources/adicionar-imagem.png')
+        self.__setIcons(self.ui.add_file,'view/forms/resources/adicionar-imagem.png')
+        self.__setIcons(self.ui.ignore_file,'view/forms/resources/adicionar-imagem.png')
+        self.__setIcons(self.ui.remove_file,'view/forms/resources/remover-imagem.png')
 
 
     def connectSignalsAndSlots(self):
         self.ui.start_button.clicked.connect(self.start_button_clicked)
         self.ui.close_button.clicked.connect(self.close_button_clicked)
         self.ui.save_button.clicked.connect(self.saveConfig_button_clicked)
+
+        self.ui.add_folder.clicked.connect(self.get_add_folder)
+        self.ui.ignore_folder.clicked.connect(self.get_ignore_folder)
+        self.ui.remove_folder.clicked.connect(self.get_remove_folder)
 
     @Slot()
     def start_button_clicked(self):
@@ -40,22 +50,34 @@ class mainWindow(QMainWindow):
             'time_unit': self.ui.time_unit_box.currentText(),
             'ui_system': self.ui.ui_system_box.currentText(),
             'ignore_images': self.ui.ignore_checkBox.isChecked(),
-            'random_display': self.ui.random_checkBox.isChecked(),
-            'mature_content': self.ui.nsfw_checkBox.isChecked()
+            'random_display': self.ui.random_checkBox.isChecked()
         }
         self.startWorker.emit(configDict)
 
-    @Slot()
+    @Slot(str)
     def close_button_clicked(self):
         self.closeWorker.emit()
 
     @Slot()
     def saveConfig_button_clicked(self):
         self.saveConfig.emit()
-    
-    # @Slot()
-    # def get_folder(self):
 
+    @Slot()
+    def get_add_folder(self):
+        self.pathOperation.emit(pathOperationType.ADD,self.__get_folder())
+
+    @Slot()
+    def get_ignore_folder(self):
+        self.pathOperation.emit(pathOperationType.IGNORE,self.__get_folder())
+
+    @Slot()
+    def get_remove_folder(self):
+        self.pathOperation.emit(pathOperationType.REMOVE,self.__get_folder())
+
+    def __get_folder(self):
+        folder_name = QFileDialog.getExistingDirectory()
+        return folder_name
+        
 
     def __readCurrentSettings(self):
         with open('../settings.json','r') as file:
@@ -67,7 +89,6 @@ class mainWindow(QMainWindow):
 
         self.ui.ignore_checkBox.setChecked(settings['ignore_images'])
         self.ui.random_checkBox.setChecked(settings['random_display'])
-        self.ui.nsfw_checkBox.setChecked(settings['mature_content'])
 
         with open('../currentSettings.json','w') as file:
             json.dump(settings,file,indent=4)
