f6c106491c4e369ec2a6d6f2caac234bbd742655
TarcizioLafaiete <tarcizio-augusto@hotmail.com>
2024-10-16 14:07:57 -0300

    build: Incrementando script de instalação para garantir o uso adequado em terminal e desktop

--- installing_script.sh
+++ installing_script.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 USER_HOME="$HOME/.WallMan"
 
@@ -8,13 +8,40 @@ fi
 
 mkdir $USER_HOME
 
-cp . $USER_HOME
+cp -r * $USER_HOME
 
-chmod +x "$USER_HOME/manager/main.py"
-chmod +x "$USER_HOME/worker/worker.py"
+if ! command -v poetry &> /dev/null
+then
+    echo "Poetry não encontrado, instalando poetry"
+    pip3 install poetry
+fi
+
+cd $USER_HOME
+poetry install
+
+DESKTOP_PATH="$HOME/.local/share/applications/Wallman.desktop"
+APP_NAME="Wallman"
+SCRIPT_PATH="$USER_HOME/Wallman.sh"
+ICON_PATH="$USER_HOME/manager/view/forms/resources/wallman_icon.jpeg"
+
+echo ${DESKTOP_PATH}
+
+echo "[Desktop Entry]
+Type=Application
+Version=0.1.0
+Name=$APP_NAME
+Comment=Gerenciador de wallpapers
+Icon=$ICON_PATH
+Exec=$SCRIPT_PATH
+Path=$USER_HOME" > $DESKTOP_PATH
+
+chmod +x $USER_HOME/Wallman.sh
 
 if !  echo "$PATH" | grep -q "$USER_HOME"; then
-    echo PATH="$PATH:$USER_HOME" >> $USER_HOME/.bashrc
+    echo PATH="$PATH:$USER_HOME" >> $HOME/.bashrc
     exec bash
 fi
+
+
+
 
